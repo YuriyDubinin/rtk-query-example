376f215c6dea47bb3e2efccbe57e3db0f2a18ab4
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-08-03 16:18:42 +0300

    Init

---
+++ src/app/App.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import {useState} from 'react';
+
+import './style/App.css';
+
+import {useGetGoodsQuery, useAddProductMutation, useDeleteProductMutation} from '../redux';
+
+
+function App() {
+  const [count, setCount] = useState('');
+  const [newProduct, setNewProduct] = useState('');
+  const {data = [], isLoading} = useGetGoodsQuery(count);
+  const [addProduct, {isError}] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const handleAddProduct = async() => {
+    if (newProduct) {
+      await addProduct({name: newProduct}).unwrap();
+
+      setNewProduct('');
+    }
+  }
+
+  const handleDeleteProduct = async(id) => {
+    await deleteProduct(id).unwrap();
+  }
+
+  return (
+    <>
+    {isLoading && (
+      <h3 className="app__label">Loading...</h3>
+    )}
+    {!isLoading && (
+      <div>
+        <h3 className="app__label">Product List:</h3>
+        <div>
+          <input 
+            className="app__input"
+            type="text" 
+            value={newProduct}
+            onChange={(e) => setNewProduct(e.target.value)}
+          />
+          <button onClick={handleAddProduct}>Add propduct</button>
+        </div>
+        <div>
+          <select 
+            value={count} 
+            onChange={(e) => setCount(e.target.value)}
+            className="app__select"
+          >
+            <option value="">all</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+        </div>
+        <ul>
+          {data.map(item => (
+            <li key={item.id} onClick={() => handleDeleteProduct(item.id)}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )}
+    </>
+  );
+}
+
+export default App;
